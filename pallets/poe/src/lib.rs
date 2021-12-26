af4ac3db950d4873c8645bc1c2e883b840b9801f
Hong Liang <lianghongbuaa@gmail.com>
2021-12-26 23:19:52 +0800

    add tests for poe pallet

--- pallets/poe/src/lib.rs
+++ pallets/poe/src/lib.rs
@@ -11,13 +11,14 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-#[cfg(feature = "runtime-benchmarks")]
-mod benchmarking;
-
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::pallet_prelude::*;
+	use frame_support::{
+		dispatch::DispatchResultWithPostInfo,
+		pallet_prelude::*
+	};
 	use frame_system::pallet_prelude::*;
+	use sp_std::vec::Vec;
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
@@ -30,73 +31,93 @@ pub mod pallet {
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
-	// The pallet's runtime storage items.
-	// https://docs.substrate.io/v3/runtime/storage
 	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u32>;
+	#[pallet::getter(fn proofs)]
+	pub type Proofs<T: Config> = StorageMap<
+		_,
+		Blake2_128Concat,
+		Vec<u8>,
+		(T::AccountId, T::BlockNumber)
+	>;
 
-	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/v3/runtime/events-and-errors
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored(u32, T::AccountId),
+		ClaimCreated(T::AccountId, Vec<u8>),
+		ClaimRevoked(T::AccountId, Vec<u8>),
 	}
 
 	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
+		ProofAlreadyExist,
+		ClaimNotExist,
+		NotClaimOwner,
 	}
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
 	// These functions materialize as "extrinsics", which are often compared to transactions.
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/v3/runtime/origins
-			let who = ensure_signed(origin)?;
 
-			// Update storage.
-			<Something<T>>::put(something);
+		// weight is related to the function executing time
+		#[pallet::weight(0)]
+		pub fn create_claim(
+			origin: OriginFor<T>,
+			claim: Vec<u8>
+		) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
 
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored(something, who));
-			// Return a successful DispatchResultWithPostInfo
-			Ok(())
+			ensure!(!Proofs::<T>::contains_key(&claim), Error::<T>::ProofAlreadyExist);
+
+			Proofs::<T>::insert(
+				&claim,
+				(sender.clone(), frame_system::Pallet::<T>::block_number())
+			);
+
+			Self::deposit_event(Event::ClaimCreated(sender, claim));
+
+			Ok(().into())
 		}
 
-		/// An example dispatchable that may throw a custom error.
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+		#[pallet::weight(0)]
+		pub fn revoke_claim(
+			origin: OriginFor<T>,
+			claim: Vec<u8>
+		) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
 
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
+			let (owner, _) = Proofs::<T>::get(&claim).ok_or(Error::<T>::ClaimNotExist)?;
+
+			ensure!(owner == sender, Error::<T>::NotClaimOwner);
+
+			Proofs::<T>::remove(&claim);
+
+			Self::deposit_event(Event::ClaimRevoked(sender, claim));
+
+			Ok(().into())
+		}
+
+		#[pallet::weight(0)]
+		pub fn transfer_claim(
+			origin: OriginFor<T>,
+			claim: Vec<u8>,
+			dest: T::AccountId
+		) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
+
+			let (owner, _block_number) = Proofs::<T>::get(&claim).ok_or(Error::<T>::ClaimNotExist)?;
+
+			ensure!(owner == sender, Error::<T>::NotClaimOwner);
+
+			// will this line override the existing one?
+			Proofs::<T>::insert(&claim, (dest, frame_system::Pallet::<T>::block_number()));
+
+			Ok(().into())
 		}
 	}
 }
--- pallets/poe/src/tests.rs
+++ pallets/poe/src/tests.rs
@@ -1,20 +1,99 @@
 use crate::{mock::*, Error};
 use frame_support::{assert_noop, assert_ok};
+use super::*;
 
 #[test]
-fn it_works_for_default_value() {
+fn create_claim_works() {
 	new_test_ext().execute_with(|| {
-		// Dispatch a signed extrinsic.
-		assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-		// Read pallet storage and assert an expected result.
-		assert_eq!(TemplateModule::something(), Some(42));
-	});
+		let claim = vec![0, 1];
+		assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+		assert_eq!(
+			Proofs::<Test>::get(&claim),
+			Some((1, frame_system::Pallet::<Test>::block_number()))
+		)
+	})
 }
 
 #[test]
-fn correct_error_for_none_value() {
+fn create_claim_failed_when_claim_already_exist() {
 	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
-		assert_noop!(TemplateModule::cause_error(Origin::signed(1)), Error::<Test>::NoneValue);
-	});
+		let claim = vec![0, 1];
+		let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+		assert_noop!(
+			PoeModule::create_claim(Origin::signed(1), claim.clone()),
+			Error::<Test>::ProofAlreadyExist
+		);
+	})
+}
+
+#[test]
+fn revoke_claim_works() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+		let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+
+		assert_ok!(PoeModule::revoke_claim(Origin::signed(1), claim.clone()));
+		assert_eq!(Proofs::<Test>::get(&claim), None);
+	})
+}
+
+#[test]
+fn revoke_claim_failed_when_claim_does_not_exist() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+
+		assert_noop!(
+			PoeModule::revoke_claim(Origin::signed(1), claim.clone()),
+			Error::<Test>::ClaimNotExist
+		);
+	})
+}
+
+#[test]
+fn revoke_claim_failed_when_the_sender_is_not_the_owner() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+		let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+
+		assert_noop!(
+			PoeModule::revoke_claim(Origin::signed(2), claim.clone()),
+			Error::<Test>::NotClaimOwner
+		);
+	})
+}
+
+#[test]
+fn transfer_claim_works() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+		let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+
+		assert_ok!(PoeModule::transfer_claim(Origin::signed(1), claim.clone(), 2));
+		assert_eq!(Proofs::<Test>::get(&claim), Some((2, frame_system::Pallet::<Test>::block_number())));
+	})
+}
+
+#[test]
+fn transfer_claim_failed_when_claim_does_not_exist() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+
+		assert_noop!(
+			PoeModule::transfer_claim(Origin::signed(1), claim.clone(), 2),
+			Error::<Test>::ClaimNotExist
+		);
+	})
+}
+
+#[test]
+fn transfer_claim_failed_when_the_sender_is_not_the_owner() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+		let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+
+		assert_noop!(
+			PoeModule::transfer_claim(Origin::signed(2), claim.clone(), 2),
+			Error::<Test>::NotClaimOwner
+		);
+	})
 }
